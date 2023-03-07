932c1d10ae75fd70330be20f30be647f737ee4be
yangzheng <snitchyang@qq.com>
2023-03-07 20:21:41 +0800

    directory updated

---
+++ src/screens/Planpage.js
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+import * as React from "react";
+
+function PlanPage() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>this is the plan page</Text>
+        </View>
+    )
+}
+
+export default PlanPage
---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+export {default as DataPage} from './Datapage'
+export {default as HomePage} from './Homepage'
+export {default as PlanPage} from './Planpage'
+export {default as DiscoveryPage} from './Discoverypage'
+export {default as ActionPage} from './Actionpage'
