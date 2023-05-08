37a889fd3a1975183c0dd563e12c0f48866e2478
“yanjs git config --global user.name “yanjs <92451186+ShadowNearby@users.noreply.github.com>
2023-05-08 20:58:40 +0800

    update statistic.html

--- api/views.py
+++ api/views.py
@@ -378,14 +378,24 @@ class statistic(APIView):
 
     def get(self, request: Request):
         user = User.objects.get(id=1)
-        diaries = Diary.objects.filter(user=user)
+        duration_type = request.query_params['duration']
+        now_date = datetime.datetime.now()
+        if duration_type == 'week':
+            ago_date = now_date - datetime.timedelta(weeks=1)
+        elif duration_type == 'month':
+            ago_date = now_date - datetime.timedelta(days=30)
+        elif duration_type == 'year':
+            ago_date = now_date - datetime.timedelta(days=365)
+        else:
+            ago_date = datetime.datetime.min
+        diaries = Diary.objects.filter(user=user, date__lte=now_date, date__gte=ago_date)
         tags = {}
         for diary in diaries:
             for tag in diary.tag.all():
                 if tag.content not in tags:
-                    tags[tag.content] = 1
+                    tags[tag.content] = round(diary.duration.hour + diary.duration.minute / 60, 2)
                 else:
-                    tags[tag.content] += 1
+                    tags[tag.content] += round(diary.duration.hour + diary.duration.minute / 60, 2)
         label = []
         data = []
         for item in sorted(tags.items(), key=lambda kv: kv[1], reverse=True):
