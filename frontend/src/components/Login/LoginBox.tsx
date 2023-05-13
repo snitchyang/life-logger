e12ac113df73535ba9b59834e0a0c5ebdd131fb2
starCloudXY <z18724628341@outlook.com>
2023-05-13 22:33:54 +0800

    update login page and register page

--- frontend/src/components/Login/LoginBox.tsx
+++ frontend/src/components/Login/LoginBox.tsx
@@ -1,57 +1,125 @@
 import React, { useState } from "react";
-import { ToastAndroid } from "react-native";
-import { View, Text, TextInput, Button,StyleSheet } from "react-native";
+import {Alert, ToastAndroid} from "react-native";
+import { View, Text, Image,TextInput,StyleSheet } from "react-native";
+import {Input,Button} from "@rneui/base";
 import {COLOR} from "../../constants";
-import {IUser} from "../../interface";
+import { FontAwesome } from '@expo/vector-icons';
 
-const RegisterForm: React.FC<IUser> = () => {
+
+
+import {IUser} from "../../interface";
+import {LoginPage} from "../../screens/Login/LoginPage";
+const RegisterForm = ({navigation}) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-
-
     const handleLogin=()=>{
-        fetch("http://10.0.2.2:8000/api/login", {
-            headers: {
-                "Content-Type": "application/json",
-            },
-            method: "POST",
-            credentials: "include",
-            body: JSON.stringify({
-                username: "123456",
-                password: "123456",
-            }),
-        })
-            .then(
-                (res) => res.json()
-            )
-            .then((res) => localStorage.setItem("token", res.token));
+        navigation.navigate("Home");
+        // fetch("http://10.0.2.2:8000/api/login", {
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     method: "POST",
+        //     credentials: "include",
+        //     body: JSON.stringify({
+        //         username: username,
+        //         password: password,
+        //     }),
+        // })
+        //     .then(
+        //         (res) => res.json()
+        //     )
+        //     .then((res) =>{
+        //         if (res.token) {
+        //             localStorage.setItem("token", res.token);
+        //             navigation.navigate("Home");
+        //         } else {
+        //             // 处理未成功获取到token的情况
+        //             Alert.alert("账号密码错误！");
+        //         }
+        //     }
+        //     );
 
 
     }
     const doReg=()=>{
-
+        navigation.navigate("Register");
     }
     const findAccount=()=>{
 
     }
     return (
-        <View style={styles.loginPage}>
+        <View >
+
+
             <View style={styles.loginSection}>
-                <Text style={styles.loginTitle}>Life Logger</Text>
-                <TextInput style={styles.loginInput} placeholder='手机号码' keyboardType={'numeric'}
-                           defaultValue={username} autoCapitalize={'none'} maxLength={11}
-                           onChangeText={(text) => setUsername(text)}/>
-                <TextInput style={styles.loginInput} placeholder='password' secureTextEntry={true}
-                           defaultValue={password} autoCapitalize={'none'} maxLength={20}
-                           onChangeText={(text) => setPassword(text)}/>
-                <Button  title={'登录'}
-                         onPress={() => handleLogin()}/>
+                <View style={{height:150,marginTop:90}}>
+                    <Image source={require('../../assets/Logo/Logo.png')} style={{
+                        width:'100%',
+                        height:'100%'
+
+                    }}/>
+                </View>
+                <Input
+                    inputContainerStyle={{marginLeft:5,marginRight:10,marginTop:20}}
+                    inputStyle={{marginLeft:15}}
+                    leftIconContainerStyle={{
+                        backgroundColor: "#ddd",
+                        margin: 0,
+                        width:40
+                    }}
+                    leftIcon={<FontAwesome
+                        name="user"
+                        color="black"
+                        size={20}
+                        margin={0}
+                    align="center"/>}
+                    placeholder='用户名'
+                    defaultValue={username}
+                    style={styles.loginInput}
+                    onChangeText={(text) => setUsername(text)}
+                labelStyle={{backgroundColor:"0xfff"}}/>
+                <Input
+                    secureTextEntry
+                    inputContainerStyle={{marginLeft:5,marginRight:10}}
+                    inputStyle={{marginLeft:15}}
+
+                    leftIconContainerStyle={{
+                        backgroundColor: "#ddd",
+                        margin: 0,
+                        width:40
+                    }}
+                    leftIcon={<FontAwesome
+                        name="lock"
+                        color="black"
+                        size={20}
+                        margin={0}
+                        align="center"/>}
+                    placeholder='密码'
+                    defaultValue={password}
+                    style={styles.loginInput}
+                    onChangeText={(text) => setPassword(text)}
+
+                    labelStyle={{backgroundColor:"0xfff"}}/>
+                <Button
+                    title="LOG IN"
+                    buttonStyle={{
+                        backgroundColor: 'black',
+                        borderWidth: 2,
+                        borderColor: 'white',
+                        borderRadius: 10,
+                        height:40
+                    }}
+                    containerStyle={{
+                        width: 300,
+                        marginVertical: 10,
+                    }}
+                    titleStyle={{ fontWeight: 'bold' }}
+                    onPress={handleLogin}
+                />
                 <View style={styles.subButton}>
                     <Text style={styles.subButtonText} onPress={() =>doReg()}>免费注册</Text>
                     <Text style={styles.subButtonText} onPress={() =>findAccount()}>找回密码</Text>
                 </View>
-                {/*<Text style={styles.message}>{message}</Text>*/}
-                {/*<Text style={{marginTop: 16, fontSize: 12}}>状态: {this.props.status}</Text>*/}
             </View>
         </View>
     );
@@ -66,6 +134,10 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         padding: 20,
         backgroundColor: COLOR.THEME_BACKGROUND
+    },
+    loginIcon:{
+        color:COLOR.THEME_TEXT,
+        size:14
     },
     loginSection: {
         flexDirection: 'column',
@@ -83,11 +155,16 @@ const styles = StyleSheet.create({
     subButton: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginTop: 8
+        marginTop: 8,
+        marginLeft:10,
+        marginRight:10,
+        alignItems:'flex-end'
     },
     subButtonText: {
         color: COLOR.THEME_TEXT,
-        fontSize: 14
+        fontSize: 14,
+        textDecorationLine:"underline",
+        alignSelf:"flex-end"
     },
     loginInput: {
         marginBottom: 8
@@ -98,5 +175,4 @@ const styles = StyleSheet.create({
         fontSize: 14
     }
 });
-
 
---
+++ frontend/src/components/Login/RegisterBox.tsx
@@ -0,0 +1,197 @@
+import React, { useState } from 'react';
+import {View, Text,  Image,Alert} from 'react-native';
+import {Input,Card,Button} from "@rneui/base";
+import {FontAwesome} from "@expo/vector-icons";
+import {Fontisto} from "@expo/vector-icons";
+
+
+export const RegisterBox = ({navigation}) => {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [verificationCode, setVerificationCode] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [gender, setGender] = useState('');
+    const [password, setPassword] = useState('');
+    const handleRegister=()=>{
+        Alert.alert("注册成功！");
+        navigation.navigate("Login");
+        fetch("http://10.0.2.2:8000/api/register", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify({
+                username:username,
+                password:password,
+                email:email,
+                gender:gender,
+                avatar:null,
+                school:null,
+                phone_number:phoneNumber
+            }),
+        })
+            .then(
+                (res) => res.json()
+            )
+            .then((res) =>{
+                if (res.message) {
+                    Alert.alert("注册成功！");
+                    navigation.navigate("Login");
+                } else {
+                    // 处理未成功获取到token的情况
+                    Alert.alert("信息错误");
+                }
+            }
+            );
+    }
+    return (
+
+        <View style={{marginTop:50}}>
+                <View style={{height:150}}>
+                    <Image source={require('../../assets/Logo/Logo.png')} style={{
+                        width:'100%',
+                        height:'100%'
+                    }}/>
+                    </View>
+            <Card
+                containerStyle={{ borderRadius: 20 }}
+                wrapperStyle={{}}
+            >
+
+                <View
+                    style={{
+                        position: "relative",
+                        alignItems: "center"
+                    }}
+                >
+                    <Input
+                        inputContainerStyle={{marginLeft:5,marginRight:10,marginTop:20,height:30}}
+                        inputStyle={{marginLeft:15,width:15,height:15}}
+                        rightIconContainerStyle={{
+                            margin: 0,
+                            width:40
+                        }}
+                        rightIcon={<FontAwesome
+                            name="user-o"
+                            color="black"
+                            size={15}
+                            margin={0}
+                            align="center"/>}
+                        placeholder='用户名'
+                        defaultValue={username}
+                        onChangeText={(text) => setUsername(text)}
+                        labelStyle={{backgroundColor:"0xfff"}}/>
+                    <Input
+                        inputContainerStyle={{marginLeft:5,marginRight:10,height:30}}
+                        inputStyle={{marginLeft:15,width:15,height:15}}
+                        rightIconContainerStyle={{
+                            margin: 0,
+                            width:40
+                        }}
+                        rightIcon={<FontAwesome
+                            name="phone"
+                            color="black"
+                            size={15}
+                            margin={0}
+                            align="center"/>}
+                        placeholder='手机号'
+                        defaultValue={phoneNumber}
+                        onChangeText={(text) => setPhoneNumber(text)}
+                        labelStyle={{backgroundColor:"0xfff"}}/>
+                    <Input
+                        inputContainerStyle={{marginLeft:5,marginRight:10,height:30}}
+                        inputStyle={{marginLeft:15,width:15,height:15}}
+                        rightIconContainerStyle={{
+                            margin: 0,
+                            width:40
+                        }}
+                        rightIcon={<FontAwesome
+                            name="lock"
+                            color="black"
+                            size={15}
+                            margin={0}
+                            align="center"/>}
+                        placeholder='请输入您的密码'
+                        defaultValue={username}
+                        onChangeText={(text) => setPassword(text)}
+                        labelStyle={{backgroundColor:"0xfff"}}/>
+                    <Input
+                        inputContainerStyle={{marginLeft:5,marginRight:10,height:30}}
+                        inputStyle={{marginLeft:15,width:15,height:15}}
+                        rightIconContainerStyle={{
+                            margin: 0,
+                            width:40
+                        }}
+                        rightIcon={<FontAwesome
+                            name="lock"
+                            color="black"
+                            size={15}
+                            margin={0}
+                            align="center"/>}
+                        placeholder='请确认您的密码'
+                        defaultValue={username}
+                        onChangeText={(text) => setPassword(text)}
+                        labelStyle={{backgroundColor:"0xfff"}}/>
+                    <Input
+                        inputContainerStyle={{marginLeft:5,marginRight:10,height:30}}
+                        inputStyle={{marginLeft:15,width:15,height:15}}
+                        rightIconContainerStyle={{
+                            margin: 0,
+                            width:40
+                        }}
+                        rightIcon={<FontAwesome
+                            name="venus-mars"
+                            color="black"
+                            size={15}
+                            margin={0}
+                            align="center"/>}
+                        placeholder='性别'
+                        defaultValue={username}
+                        onChangeText={(text) => setPassword(text)}
+                        labelStyle={{backgroundColor:"0xfff"}}/>
+                    <Input
+                        inputContainerStyle={{marginLeft:5,marginRight:10,height:30}}
+                        inputStyle={{marginLeft:15,width:15,height:15}}
+                        rightIconContainerStyle={{
+                            margin: 0,
+                            width:40
+                        }}
+                        rightIcon={<Fontisto
+                            name="email"
+                            color="black"
+                            size={15}
+                            margin={0}
+                            align="center"/>}
+                        placeholder='邮箱'
+                        defaultValue={email}
+                        onChangeText={(text) => setEmail(text)}
+                        labelStyle={{backgroundColor:"0xfff"}}/>
+
+                </View>
+            </Card>
+            <Button
+                title="注册"
+                buttonStyle={{
+                    backgroundColor: 'black',
+                    borderWidth: 2,
+                    borderColor: 'white',
+                    borderRadius: 10,
+                    width:300,
+                    height:40,
+                    alignSelf:'center'
+                }}
+                containerStyle={{
+                    width: 300,
+                    marginVertical: 10,
+                    alignItems:'center',
+                    alignSelf:'center'
+                }}
+                titleStyle={{ fontWeight: 'bold' }}
+                onPress={handleRegister}
+            />
+        </View>
+    );
+};
+
+export default RegisterBox;
